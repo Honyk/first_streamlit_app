535d12b154b38ffcc3d7fea8046ab9acf32d329d
Honyk <127707360+Honyk@users.noreply.github.com>
2023-06-27 08:58:15 +1200

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,6 @@ def get_fruityvice_data(this_fruit_choice):
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
 
-#except URLError as e:
-#streamlit.error()  
-
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -46,8 +43,8 @@ try:
 
 # Don't run anything past here while we troubleshoot
 except URLError as e:
-streamlit.stop()
-
+      streamlit.error()  
+      
 # import snowflake.connector
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
@@ -56,14 +53,14 @@ def get_fruit_load_list();
            my_cur.execute("SELECT * from fruit_load_list")      
            return my_cur.fetchall()
 
-#add a button
+streamlit.stop()
+
+# add a button
 if streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 
-#except URLError as e:
-  streamlit.error()  
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit) 
